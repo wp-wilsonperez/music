21d3d30d8cbd9f546cae855d19d25c02dada9e93
wp_wilsonperez <wilsonperez.developer@gmail.com>
2018-01-25 11:19:41 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default{
+  appName:'platzimusicwilson',
+  apiKey: '514cbc2d073c78b060499e0c3372b6f9',
+  secret: '16295b73fcfc4d07dc62156aee7af781',
+  registeredTo: 'wilsonware'
+}
+
+//Application name	platzimusicwilson
+//API key	514cbc2d073c78b060499e0c3372b6f9
+//Shared secret	16295b73fcfc4d07dc62156aee7af781
+//Registered to	wilsonware
